b2617b87833850f87ddf0a0db74e5b9bc078144f
Revathii <srevathi036@gmail.com>
2016-09-19 17:02:45 +0530

    delete todos

--- undescore.js
+++ undescore.js
@@ -49,6 +49,19 @@ app.post('/todos', function(req, res){
 
 });
 
+// Delete /todos/:id
+
+app.delete('/todos/:id', function(req, res){
+	var todoId = parseInt(req.params.id);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	if(!matchedTodo) {
+		res.status(404).json({"error":"no todo found with that id"});
+	} else {
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	}
+})
+
 app.listen(PORT, function(){
 	console.log(' Express Listening on port ' + PORT + '!!');
 });
